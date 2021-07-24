694232ae19ed5846bb6f99fb63e59d78ac0cea2c
Kevin Willian <kevinwillian084@gmail.com>
2021-07-24 01:35:28 -0300

    add basic pokemon's information

--- src/pages/pokemonDetails/componentsPokemonDetails/PokemonFoundAt.js
+++ src/pages/pokemonDetails/componentsPokemonDetails/PokemonFoundAt.js
@@ -4,7 +4,7 @@ class PokemonFoundAt extends Component {
   render() {
     const mapa = this.props.pokemon.foundAt;
     return mapa.map((mapinha) => (
-      <ul>
+      <ul key={mapinha.location}>
         <li>{mapinha.location}</li>
         <img src={mapinha.map}></img >
       </ul>
